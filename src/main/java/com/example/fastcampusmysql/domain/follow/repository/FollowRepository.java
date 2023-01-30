13e96fee0c219a12839479beaffa4c214b061b8d
yongjae_park <ytothej92@gmail.com>
2023-01-30 18:48:43 +0900

    feat(FollowRepository) : findAllByFromMemberId 메소드 추가

--- src/main/java/com/example/fastcampusmysql/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/follow/repository/FollowRepository.java
@@ -2,11 +2,17 @@ package com.example.fastcampusmysql.domain.follow.repository;
 
 import com.example.fastcampusmysql.domain.follow.entity.Follow;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,7 +20,20 @@ public class FollowRepository {
 
     static final String TABLE = "follow";
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    static final RowMapper<Follow> ROW_MAPPER = (ResultSet resultSet, int rowNum) -> Follow.builder()
+            .id(resultSet.getLong("id"))
+            .fromMemberId(resultSet.getLong("fromMemberId"))
+            .toMemberId(resultSet.getLong("toMemberId"))
+            .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
+    public List<Follow> findAllByFromMemberId(Long fromMemberId) {
+        String sql = String.format("SELECT * FROM %s WHERE fromMemberId = :fromMemberId", TABLE);
+        MapSqlParameterSource params = new MapSqlParameterSource().addValue("fromMemberId", fromMemberId);
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
 
     public Follow save(Follow follow) {
         if (follow.getId() == null)
@@ -23,7 +42,7 @@ public class FollowRepository {
         throw new UnsupportedOperationException("Follow는 갱신을 지원하지 않습니다.");
     }
     private Follow insert(Follow follow) {
-        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
                 .withTableName(TABLE)
                 .usingGeneratedKeyColumns("id");
 
