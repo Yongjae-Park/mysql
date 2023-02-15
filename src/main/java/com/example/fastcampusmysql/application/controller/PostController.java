4eeb6fcd412679a8b53bff00e9117de7d0471e45
yongjae_park <ytothej92@gmail.com>
2023-02-15 17:15:43 +0900

    feat(PostController) : member조회 페이징 api추가

--- src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
+++ src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -3,11 +3,15 @@ package com.example.fastcampusmysql.application.controller;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostDto;
+import com.example.fastcampusmysql.domain.post.entity.Post;
 import com.example.fastcampusmysql.domain.post.service.PostReadService;
 import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -28,6 +32,14 @@ public class PostController {
     @GetMapping("/daily-post-counts")
     public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
         return postReadService.getDailyPostCounts(request);
+    }
+
+    @GetMapping("/members/{memberId}")
+    public Page<Post> getPost(
+            @PathVariable Long memberId,
+            Pageable pageable
+    ) {
+        return postReadService.getPosts(memberId, pageable);
     }
 
 }
