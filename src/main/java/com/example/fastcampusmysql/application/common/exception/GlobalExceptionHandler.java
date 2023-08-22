9f8056c543d5f3f1c19deb4dd8e295167183e041
yongjae_park <ytothej92@gmail.com>
2023-08-22 16:30:03 +0900

    refactor: 예외처리 핸들러 예외 생성 부분 수정
    예외 응답 필드 추가

--- src/main/java/com/example/fastcampusmysql/application/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/fastcampusmysql/application/common/exception/GlobalExceptionHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,21 +19,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateEmailException.class)
     public final ResponseEntity<ErrorResponse> DuplicateEmailExceptionHandler(DuplicateEmailException exception) {
         String message = exception.getMessage();
-        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(message);
+        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(LocalDateTime.now(), message, exception.getStackTrace().toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(DuplicateNicknameException.class)
     public final ResponseEntity<ErrorResponse> DuplicationNicknameExceptionHandler(DuplicateNicknameException exception) {
         String message = exception.getMessage();
-        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(message);
+        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(LocalDateTime.now(), message, exception.getStackTrace().toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public final ResponseEntity<ErrorResponse> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
         String message = exception.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(message);
+        ErrorResponse errorResponse = ExceptionHandlerUtil.createError(LocalDateTime.now(), message, exception.getStackTrace().toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
--- src/main/java/com/example/fastcampusmysql/application/utils/ExceptionHandlerUtil.java
+++ src/main/java/com/example/fastcampusmysql/application/utils/ExceptionHandlerUtil.java
@@ -3,12 +3,17 @@ package com.example.fastcampusmysql.application.utils;
 
 import com.example.fastcampusmysql.application.common.exception.ErrorResponse;
 
+import java.time.LocalDateTime;
+
 public class ExceptionHandlerUtil {
 
-    public static ErrorResponse createError(String message) {
+    public static ErrorResponse createError(LocalDateTime timestamp, String message, String trace) {
         ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(timestamp)
                 .message(message)
+                .trace(trace)
                 .build();
+
         return errorResponse;
     }
 }
