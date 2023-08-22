adeaa8fe19e671a721f84c13b6fdc21ec3c95f27
yongjae_park <ytothej92@gmail.com>
2023-08-22 17:06:39 +0900

    refactor: 좋아요 insert메소드 리턴 파라미터 수정

--- src/main/java/com/example/fastcampusmysql/domain/post/repository/PostLikeRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostLikeRepository.java
@@ -1,6 +1,6 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
-import com.example.fastcampusmysql.domain.post.entity.PostLike;
+import com.example.fastcampusmysql.domain.post.entity.Post;
 import com.example.fastcampusmysql.domain.post.entity.PostLike;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -25,6 +24,7 @@ public class PostLikeRepository {
     final static private RowMapper<PostLike> ROW_MAPPER = (ResultSet resultSet, int rowNum) -> PostLike.builder()
             .id(resultSet.getLong("id"))
             .memberId(resultSet.getLong("memberId"))
+//            .post(resultSet.getObject("post", Post.class))
             .postId(resultSet.getLong("postId"))
             .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
             .build();
@@ -35,21 +35,22 @@ public class PostLikeRepository {
         throw new UnsupportedOperationException("Post는 갱신을 지원하지 않습니다.");
     }
 
-    private PostLike insert(PostLike PostLike) {
+    private PostLike insert(PostLike postLike) {
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
                 .withTableName(TABLE)
                 .usingGeneratedKeyColumns("id");
 
-        SqlParameterSource params = new BeanPropertySqlParameterSource(PostLike);
+        SqlParameterSource params = new BeanPropertySqlParameterSource(postLike);
         Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
 
         return PostLike.builder()
                 .id(id)
-                .memberId(PostLike.getMemberId())
-                .createdAt(PostLike.getCreatedAt())
+                .memberId(postLike.getMemberId())
+                .postId(postLike.getPostId())
+//                .post(postLike.getPost())
+                .createdAt(postLike.getCreatedAt())
                 .build();
     }
-
     public Long count(Long postId) {
         String sql = String.format("""
                 SELECT count(id)
