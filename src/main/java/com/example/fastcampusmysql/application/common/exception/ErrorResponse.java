43aa82941d71588d846968fa33b3657c3b3863f6
yongjae_park <ytothej92@gmail.com>
2023-08-22 22:52:33 +0900

    refactor: 응답처리 오브젝트 필드 추가
    timestamp, trace 추가

--- src/main/java/com/example/fastcampusmysql/application/common/exception/ErrorResponse.java
+++ src/main/java/com/example/fastcampusmysql/application/common/exception/ErrorResponse.java
@@ -3,8 +3,14 @@ package com.example.fastcampusmysql.application.common.exception;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class ErrorResponse {
     private String message;
+
+    private LocalDateTime timestamp;
+
+    private String trace;
 }
