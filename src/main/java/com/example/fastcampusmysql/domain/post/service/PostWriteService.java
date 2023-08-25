86fcfae648aecd236ff5dcb5cd89398b8803e829
yongjae_park <ytothej92@gmail.com>
2023-08-25 23:02:02 +0900

    refactor: PostWriteService 의존 Repository 변경
    PostJpaRepository사용

--- src/main/java/com/example/fastcampusmysql/domain/post/service/PostWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostWriteService.java
@@ -1,7 +1,11 @@
 package com.example.fastcampusmysql.domain.post.service;
 
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.repository.MemberJpaRepository;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
 import com.example.fastcampusmysql.domain.post.dto.PostDto;
 import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostJpaRepository;
 import com.example.fastcampusmysql.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,27 +17,36 @@ public class PostWriteService {
     /*
         TODO: 게시물 회원 검증
      */
+    final private PostJpaRepository postJpaRepository;
+
     final private PostRepository postRepository;
 
-    public Long create(PostDto postDto) {
-        Post post = Post.builder()
-                .member(postDto.member())
-                .contents(postDto.contents())
-                .build();
-
-        return postRepository.save(post).getId();
-    }
+    final private MemberJpaRepository memberJpaRepository;
 
     @Transactional
+    public Long create(PostDto postDto) {
+        Member findMember = memberJpaRepository.findById(postDto.memberId()).orElseThrow();
+
+        Post post = Post.builder()
+                .member(findMember)
+                .contents(postDto.contents())
+                .likeCount(0L)
+                .build();
+
+        return postJpaRepository.save(post).getId();
+    }
+
+    //TODO : 낙관적 락 좋아요기능 JpaRepository 사용하여 구현
+    @Transactional
     public void likePost(Long postId) {
-        Post post = postRepository.findById(postId, true).orElseThrow();
+        Post post = postJpaRepository.findById(postId).orElseThrow();
         post.incrementLikeCount();
-        postRepository.save(post);
+        postJpaRepository.save(post);
     }
 
     public void likePostByOptimisticLock(Long postId) {
-        Post post = postRepository.findById(postId, false).orElseThrow();
+        Post post = postJpaRepository.findById(postId).orElseThrow();
         post.incrementLikeCount();
-        postRepository.save(post);
+        postJpaRepository.save(post);
     }
 }
